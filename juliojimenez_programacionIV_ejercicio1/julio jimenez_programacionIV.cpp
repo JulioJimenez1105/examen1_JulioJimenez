f13c7b2807cd753f00a800f678e054a932161a45
JulioJimenez1105 <82010609+JulioJimenez1105@users.noreply.github.com>
2021-04-30 23:05:05 -0400

    Add files via upload

---
+++ juliojimenez_programacionIV_ejercicio1/julio jimenez_programacionIV.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+
+
+ int numero,resto;
+
+ cout << "Ingrese el numero: ";
+
+ cin >> numero;
+
+
+
+ resto=numero%2;
+
+
+
+ if (resto==0) cout << "el numero es par";
+
+ else cout << "el numero es impar";
+
+
+
+ return 0;
+
+}
